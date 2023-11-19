443a28d8298185eb6554200ac79427506d71b71a
Jakub Samul <jakubsamul0@gmail.com>
2023-11-19 19:21:03 +0100

    'data_classes_in_backpack'

--- zadanie_plecak.py
+++ zadanie_plecak.py
@@ -1,14 +1,15 @@
-# # class Item:
+from dataclasses import dataclass
 
-# #     def __init__(self, weight, value): 
-# #         self.weight = weight 
-# #         self.value = value 
+@dataclass
+class Item:
+    weight: int
+    value: int
     
-# # watch = Item(25,75)
-# # ring = Item(26,90)
-# # book = Item(51,200)
+watch = Item(25,75)
+ring = Item(26,90)
+book = Item(51,200)
 
-# # # watch = 3 ring = 3,46 book = 3,92 pojemność = 100
+print(watch, ring, book)
 
 # # def pack_backpack(items: list[Item], weight):
 # #     pass
@@ -26,12 +27,12 @@
 
 # print(suma(5))
 
-def suma(a):
-    if a < 0:
-        return 0 
-    else:
-        return a + suma(a-1)
+# def suma(a):
+#     if a < 0:
+#         return 0 
+#     else:
+#         return a + suma(a-1)
 
-print(suma(5))
+# print(suma(5))
     # 0+1+2+3+4+5   5+4+3+2+1+0
 
