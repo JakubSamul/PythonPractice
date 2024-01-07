20fd7eed5b851adc01c5fffc91332191dd46b9ce
Jakub Samul <jakubsamul0@gmail.com>
2024-01-07 18:37:20 +0100

    fastapi_tutorial-3

--- fastapi_tutorial/main.py
+++ fastapi_tutorial/main.py
@@ -59,3 +59,40 @@ async def get_food(food_name: FoodEnum):
     }
 
 
+fake_items_db = [
+    {"item_name": "Foo"},
+    {"item_name": "Bar"},
+    {"item_name": "Baz"}
+    ]
+
+@app.get("/items")
+async def list_items(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+
+@app.get("/items/{item_id}")
+async def get_item(item_id: str, sample_query_param: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id, "sample_query_param": sample_query_param}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {
+                "description": "blaa blaa blaa"
+            }
+        )
+    return item
+
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def get_user_id(user_id: int, item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id, "user_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {
+                "description": "blaa blaa blaa"
+            }
+        )
+    return item
