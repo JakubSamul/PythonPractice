7fd44bb77caf95f946861f16c01f4cbf4e45acc4
Jakub Samul <jakubsamul0@gmail.com>
2024-01-11 19:45:17 +0100

    fastapi-tutoroal-9

--- fastapi_tutorial/main.py
+++ fastapi_tutorial/main.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 from fastapi import FastAPI, Query, Path, Body
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, HttpUrl
 
 app = FastAPI()
 
@@ -185,13 +185,55 @@ app = FastAPI()
 #     return result
 
 # Part 8 -> Body -> Field
+# class Item(BaseModel):
+#     name: str
+#     description: str | None = Field(None, title="The description of the item", max_length=300)
+#     price: float = Field(..., gt=0, description="The price must be greater than zero.")
+#     tax: float | None = None
+
+# @app.put("/items/{item_id}")
+# async def update_item(item_id: int, item: Item = Body(..., embed=True)):
+#     result = {"item_id": item_id, "item": item}
+#     return result
+
+# Part 9 -> Body -> Nasted Models
+class Image(BaseModel):
+    url: HttpUrl
+    name: str
+
+
 class Item(BaseModel):
     name: str
-    description: str | None = Field(None, title="The description of the item", max_length=300)
-    price: float = Field(..., gt=0, description="The price must be greater than zero.")
+    description: str | None = None
+    price: float
     tax: float | None = None
+    tags: set[str] = []
+    image: Image | None = None
+
+
+class Offer(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    item: list[Item]
+
 
 @app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item = Body(..., embed=True)):
+async def update_item(item_id: int, item: Item):
     result = {"item_id": item_id, "item": item}
     return result
+
+
+@app.post("/offers")
+async def cretae_offer(offer: Offer = Body(..., embed=True)):
+    return offer
+
+
+@app.post("/images/multiple")
+async def create_multiple_images(images: list[Image]):
+    return images
+
+
+@app.post("/blah")
+async def create_some_blahs(blahs: dict[int, float]):
+    return blahs
