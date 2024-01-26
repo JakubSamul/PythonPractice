7848c28e60a92d7a4c63a67fd4e4a0895e401873
Jakub Samul <jakubsamul0@gmail.com>
2024-01-26 17:24:13 +0100

    UnitTest-by-Cory-part1

---
+++ Unit_Test/calc.py
@@ -0,0 +1,13 @@
+def add(a,b):
+    return a + b
+
+def subtract(x,y):
+    return x - y
+
+def multiply(x,y):
+    return x * y
+
+def divide(x,y):
+    if y == 0: 
+        raise ValueError('Can not divide by zero')
+    return x / y
