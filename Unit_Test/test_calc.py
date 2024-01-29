31181f6f28e8f8585322173c334d0e95e703f1c1
Jakub Samul <jakubsamul0@gmail.com>
2024-01-29 19:59:03 +0100

    UnitTest-byCorey-Part2

--- Unit_Test/test_calc.py
+++ Unit_Test/test_calc.py
@@ -24,7 +24,8 @@ class TestCalc(unittest.TestCase):
         self.assertEqual(calc.divide(-1,1), -1)
         self.assertEqual(calc.divide(-1,-1), 1)
 
-        self.assertRaises(ValueError, calc.divide, 10, 0)
+        with self.assertRaises(ValueError):
+            calc.divide(10, 0)
 
 if __name__ == '__main__':
     unittest.main()
