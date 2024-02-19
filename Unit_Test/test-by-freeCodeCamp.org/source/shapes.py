c702e34ff630bdd14816335b5540ae21a129de1e
Jakub Samul <jakubsamul0@gmail.com>
2024-02-19 18:48:57 +0100

    learn-pytest-whit-freeCodeCamp-part3

--- Unit_Test/test-by-freeCodeCamp.org/source/shapes.py
+++ Unit_Test/test-by-freeCodeCamp.org/source/shapes.py
@@ -20,3 +20,16 @@ class Circle(Shape):
     
     def parimeter(self):
         return 2 * math.pi * self.radius
+    
+
+class Rectangle(Shape):
+
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+    
+    def perimeter(self):
+        return(self.length * 2) + (self.width * 2)
