032cb94e539d6475e9e76258ea3eceb28d75d721
Jakub Samul <jakubsamul0@gmail.com>
2024-02-19 19:23:22 +0100

    fastapi-tutorial-part23

--- fastapi_tutorial/main.py
+++ fastapi_tutorial/main.py
@@ -716,22 +716,43 @@ app = FastAPI()
 #     return update_item
 
 
-# 22 - Dependencies Intro
-async def hello():
-    return "world"
+# # 22 - Dependencies Intro
+# async def hello():
+#     return "world"
 
 
-async def comon_parameters(
-    q: str | None = None, skip: int = 0, limit: int = 100, blah: str = Depends(hello)
-):
-    return {"q": q, "skip": skip, "limit": limit, "hello": blah}
+# async def comon_parameters(
+#     q: str | None = None, skip: int = 0, limit: int = 100, blah: str = Depends(hello)
+# ):
+#     return {"q": q, "skip": skip, "limit": limit, "hello": blah}
 
 
-@app.get("/items/")
-async def read_items(commons: dict = Depends(comon_parameters)):
-    return commons
+# @app.get("/items/")
+# async def read_items(commons: dict = Depends(comon_parameters)):
+#     return commons
 
 
-@app.get("/users/")
-async def read_users(commons: dict = Depends(comon_parameters)):
-    return commons
+# @app.get("/users/")
+# async def read_users(commons: dict = Depends(comon_parameters)):
+#     return commons
+
+
+# 23 - Classes as Dependencies
+fake_items_db = [{'item_name': 'Foo'}, {'item_name': 'Bar'}, {'item_name': 'Baz'}]
+
+
+class ComonQueryParams:
+    def __init__(self, q: str | None = None, skip: int = 0, limit: int = 100):
+        self.q = q
+        self.skip = skip
+        self.limit = limit
+
+    
+@app.get('/items/{item_id}')
+async def read_items(commons: ComonQueryParams = Depends()):
+    response = {}
+    if commons.q:
+        response.update({'q': commons.q})
+    items = fake_items_db[commons.skip: commons.skip + commons.limit]
+    response.update({'items': items})
+    return response 
