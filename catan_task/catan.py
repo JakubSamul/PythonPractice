a4dff986b2518777f83eba36ca0cb1176c7aeb12
Jakub Samul <jakubsamul0@gmail.com>
2024-03-04 20:36:42 +0100

    cd

--- catan_task/catan.py
+++ catan_task/catan.py
@@ -2,13 +2,13 @@ from enum import Enum
 from fastapi import FastAPI, Path, Query
 from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
+import random
 
 
 card = FastAPI()
 
 
-class Card(BaseModel):
-    cards: list[str] = [
+list_of_vc = [
         'k', 'k', 'k', 'k', 'k', 'k', 'k', 'k', 'k', 'k', 'k', 'k', 'k', 'k', 'k', 'k', 'k', 'k', 'k', 'k',
         'vp', 'vp', 'vp', 'vp', 'vp', 'v', 'v', 'v', 'br', 'br', 'br', 'm', 'm', 'm'
     ]
@@ -16,17 +16,18 @@ class Card(BaseModel):
 
 class Player(BaseModel):
     name: str | None = None
-    card: list[str] | None = None
+    cards: list[str] | None = []
 
 
 players = {}
 
-def grab_card(player_id: int, card: Card):
-    pass
 
-@card.get("/get_player/{player_id}")
-async def read_player(player_id: int):
-    return players.get(player_id)
+def t_card():
+    take = random.choice(list_of_vc)
+    list_of_vc.remove(take)
+    return take
+
+print(t_card())
 
 
 @card.post("/create_player/{player_id}")
@@ -34,3 +35,14 @@ async def create_player(player_id: int, player: Player):
     players[player_id] = player
     return players[player_id]
 
+
+@card.get("/get_player/{player_id}")
+async def read_player(player_id: int):
+    return players.get(player_id)
+
+
+@card.put('/grab_card/{player_id}')
+async def grab_card(player_id: int):
+    player = players[player_id]
+    player.cards = [t_card]
+    return player
