6d3d074496962407af25df797c28629a93281039
Jakub Samul <jakubsamul0@gmail.com>
2024-03-18 20:59:32 +0100

    why-react-2

--- React Course/index.js
+++ React Course/index.js
@@ -1,12 +1,15 @@
-function MainContent() {
-    return (
-        <h1>I'm learning React!</h1>
-    )
-}
+// function MainContent() {
+//     return (
+//         <h1>I'm learning React!</h1>
+//     )
+// }
 
 ReactDOM.render(
-    <div>
-        <MainContent />
-    </div>, 
+    <h1 className="header">This is an imperative way to program</h1>, 
     document.getElementById("root")
 )
+
+// const h1 = document.createElement("h1")
+// h1.textContent = "This is an imperative way to program"
+// h1.className = "header"
+// document.getElementById("root").append(h1)
