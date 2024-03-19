9a12c8198c053cd189c3f8974a8b8666219787ae
Jakub Samul <jakubsamul0@gmail.com>
2024-03-19 20:44:13 +0100

    Thought-Experiment

--- React Course/index.js
+++ React Course/index.js
@@ -1,27 +1,11 @@
-// const h1 = document.createElement("h1")
-// h1.textContent = "This is an imperative way to program"
-// h1.className = "header"
-// console.log(h1)
-
-
-// JSX
-// const page = (
-//     <div>
-//         <h1 className="header">This is JSX</h1>
-//         <p>This is a paragraph</p>
-//     </div>
-// )
-
 const navbar = (
     <nav>
         <h1>Jackobo Restaurant</h1>
+        <h3>Ohohoho I am Santa xD</h3>
         <li>Menu</li>
         <li>Drink</li>
         <li>Food</li>
     </nav>
 )
 
-ReactDOM.render(
-    navbar,
-    document.getElementById("root")
-)
+ReactDOM.render(navbar, document.getElementById("root"))
