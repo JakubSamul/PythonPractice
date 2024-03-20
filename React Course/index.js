809c0f0e2b6335ce070a30ac6c40ef77d35271ce
Jakub Samul <jakubsamul0@gmail.com>
2024-03-20 21:15:22 +0100

    Project-1-Part-1-MarkUp

--- React Course/index.js
+++ React Course/index.js
@@ -1,11 +1,15 @@
-const navbar = (
-    <nav>
-        <h1>Jackobo Restaurant</h1>
-        <h3>Ohohoho I am Santa xD</h3>
-        <li>Menu</li>
-        <li>Drink</li>
-        <li>Food</li>
-    </nav>
+const page = (
+    <div>
+        <img src="./react-logo.png" width="40px" />
+        <h1>Fun fact about React</h1>
+        <ul>
+            <li>Was first relased in 2013</li>
+            <li>Was orginally created by Jordan Walke</li>
+            <li>Has well over 100K stars on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+    </div>
 )
 
-ReactDOM.render(navbar, document.getElementById("root"))
+ReactDOM.render(page, document.getElementById("root"))
