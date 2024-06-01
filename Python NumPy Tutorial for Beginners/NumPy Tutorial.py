d66fc1f3ed06a82cc0650a987ff5c3b43a9482e7
JakubSamul <103068467+JakubSamul@users.noreply.github.com>
2024-06-01 13:10:42 +0200

    NumPy-Basics

---
+++ Python NumPy Tutorial for Beginners/NumPy Tutorial.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+# The Basics
+
+# a = np.array([1, 2, 3])
+
+# b = np.array([[9.0, 8.0, 7.0], [6.0, 5.0, 4.0]])
+# print(b)
+
+# # Get Dimension
+# print(a.ndim)
+
+# # Get Shape
+# print(b.shape)
+
+# # Get type
+# print(b.dtype)
+
+# # Get Size
+# print(a.itemsize)
+
+# # Get total size
+# print(a.nbytes)
