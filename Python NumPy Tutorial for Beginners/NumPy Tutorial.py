2bbbb871ad9818d2d46484b432ec535a1f1eb1df
JakubSamul <103068467+JakubSamul@users.noreply.github.com>
2024-06-01 19:08:02 +0200

    Mathematics

--- Python NumPy Tutorial for Beginners/NumPy Tutorial.py
+++ Python NumPy Tutorial for Beginners/NumPy Tutorial.py
@@ -53,52 +53,107 @@ import numpy as np
 # b[:, 1, :] = [[9, 9], [8, 8]]
 
 
-# Initializing Different Types of Arrays
+# # Initializing Different Types of Arrays
 
-# All 0s matrix
-np.zeros((2, 3))
+# # All 0s matrix
+# np.zeros((2, 3))
 
-# All 1s matrix
-np.ones((4, 2, 2), dtype="int32")
+# # All 1s matrix
+# np.ones((4, 2, 2), dtype="int32")
 
-# Any other number
-np.full((2, 2), 99)
+# # Any other number
+# np.full((2, 2), 99)
 
-# Any other number (full_like)
-np.full_like(a, 4)
+# # Any other number (full_like)
+# np.full_like(a, 4)
 
-# Random decimal numbers
-np.random.rand(4, 2)
+# # Random decimal numbers
+# np.random.rand(4, 2)
 
-# Random Integer values
-np.random.randint(-4, 8, size=(3, 3))
+# # Random Integer values
+# np.random.randint(-4, 8, size=(3, 3))
 
-# The identity matrix
-np.identity(5)
+# # The identity matrix
+# np.identity(5)
 
-# Repeat an array
-arr = np.array([[1, 2, 3]])
-r1 = np.repeat(arr, 3, axis=0)
-print(r1)
+# # Repeat an array
+# arr = np.array([[1, 2, 3]])
+# r1 = np.repeat(arr, 3, axis=0)
+# print(r1)
 
-# [[1. 1. 1. 1. 1.]
-#  [1. 0. 0. 0. 1.]
-#  [1. 0. 9. 0. 1.]
-#  [1. 0. 0. 0. 1.]
-#  [1. 1. 1. 1. 1.]]
-output = np.ones((5, 5))
-print(output)
+# # [[1. 1. 1. 1. 1.]
+# #  [1. 0. 0. 0. 1.]
+# #  [1. 0. 9. 0. 1.]
+# #  [1. 0. 0. 0. 1.]
+# #  [1. 1. 1. 1. 1.]]
+# output = np.ones((5, 5))
+# print(output)
 
-z = np.zeros((3, 3))
-z[1, 1] = 9
-print(z)
+# z = np.zeros((3, 3))
+# z[1, 1] = 9
+# print(z)
 
-output[1:-1, 1:-1] = z
-print(output)
+# output[1:-1, 1:-1] = z
+# print(output)
 
-# Be careful when copying arrays!!!
-a = np.array([1, 2, 3])
-b = a.copy()
-b[0] = 100
+# # Be careful when copying arrays!!!
+# a = np.array([1, 2, 3])
+# b = a.copy()
+# b[0] = 100
 
+# print(a)
+
+
+# Mathematics
+
+a = np.array([1, 2, 3, 4])
+
+
+a + 2
+
+a - 2
+
+a * 2
+
+a / 2
+
+b = np.array([1, 0, 1, 0])
+a + b
+
+a**2
+
+# Take the sin
+np.sin(a)
+
+# For a lot more (https://docs.scipy.org/doc/numpy/reference/routines.math.html)
+
+# Linear Algebra
+a = np.ones((2, 3))
 print(a)
+
+b = np.full((3, 2), 2)
+print(b)
+
+np.matmul(a, b)
+
+# Find the determinant
+c = np.identity(3)
+np.linalg.det(c)
+
+## Reference docs (https://docs.scipy.org/doc/numpy/reference/routines.linalg.html)
+
+# Determinant
+# Trace
+# Singular Vector Decomposition
+# Eigenvalues
+# Matrix Norm
+# Inverse
+# Etc...
+# Statistics
+stats = np.array([[1, 2, 3], [4, 5, 6]])
+
+np.min(stats)
+
+np.max(stats, axis=1)
+
+np.sum(stats, axis=0)
