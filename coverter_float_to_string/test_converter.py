bc6b92c45d4f8a9f93256c262b405041133016cb
JakubSamul <jakubsamul0@gmail.com>
2024-07-31 17:41:47 +0200

    add-test_negative_integer

--- coverter_float_to_string/test_converter.py
+++ coverter_float_to_string/test_converter.py
@@ -11,6 +11,9 @@ class TestConverterFloatToString(unittest.TestCase):
     def test_positive_float(self):
         self.assertEqual(float_to_string_converter(2000.22), "2 000.22")
 
+    def test_negative_iteger(self):
+        self.assertEqual(float_to_string_converter(-2000.0), "-2 000")
+
 
 if __name__ == "__main__":
     unittest.main()
