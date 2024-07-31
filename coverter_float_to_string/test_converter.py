2cdf09069ca8269987f4f973724ce59768bcd2d1
JakubSamul <jakubsamul0@gmail.com>
2024-07-31 17:43:44 +0200

    add-test_zero

--- coverter_float_to_string/test_converter.py
+++ coverter_float_to_string/test_converter.py
@@ -17,6 +17,9 @@ class TestConverterFloatToString(unittest.TestCase):
     def test_negative_float(self):
         self.assertEqual(float_to_string_converter(-2000.22), "-2 000.22")
 
+    def test_zero(self):
+        self.assertEqual(float_to_string_converter(0.0), "0")
+
 
 if __name__ == "__main__":
     unittest.main()
