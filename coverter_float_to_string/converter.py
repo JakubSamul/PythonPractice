ee3929b65060811210c60bc0c66514348dcd0409
JakubSamul <jakubsamul0@gmail.com>
2024-07-31 19:39:14 +0200

    add-functionality

--- coverter_float_to_string/converter.py
+++ coverter_float_to_string/converter.py
@@ -1,2 +1,17 @@
 def float_to_string_converter(value: float) -> str:
-    return ""
+    if value == float("inf"):
+        return "inf"
+    elif value == float("-inf"):
+        return "-inf"
+    elif value != value:
+        return "NaN"  # Not a number
+
+    if value == int(value):
+        return f"{int(value):,}".replace(",", " ")
+    else:
+        return f"{value:,.2f}".replace(",", " ")
+
+
+print(float_to_string_converter(2000000.22))
+print(float_to_string_converter(200000.228))
+print(float_to_string_converter(2000))
