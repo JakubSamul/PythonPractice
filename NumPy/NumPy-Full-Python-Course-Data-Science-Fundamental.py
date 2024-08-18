7a80c54feb292e9166c6c17ffd7a2b2ebaa80c0b
JakubSamul <jakubsamul0@gmail.com>
2024-08-18 14:58:10 +0200

    numpay-data types

--- NumPy/NumPy-Full-Python-Course-Data-Science-Fundamental.py
+++ NumPy/NumPy-Full-Python-Course-Data-Science-Fundamental.py
@@ -14,10 +14,34 @@ import numpy as np
 # a[2] = 100
 # print(a)
 
-a_nul = np.array([[[1, 2, 3, 1], [4, 5, 6, 1], [7, 8, 9, 1]], [[1, 2, 3, 1], [4, 5, 6, 1], [7, 8, 9, 1]]])
-print(a_nul[0])
-print(a_nul[0, 1])
-print(a_nul.shape)
-print(a_nul.ndim)
-print(a_nul.size)
-print(a_nul.dtype)
+# a_nul = np.array(
+#     [
+#         [[1, 2, 3, 1], [4, 5, 6, 1], [7, 8, 9, 1]],
+#         [[1, 2, 3, 1], [4, 5, 6, 1], [7, 8, 9, 1]],
+#     ]
+# )
+# print(a_nul[0])
+# print(a_nul[0, 1])
+# print(a_nul.shape)
+# print(a_nul.ndim)
+# print(a_nul.size)
+# print(a_nul.dtype)
+
+# Data Types
+
+# a_niedziala = np.array(
+#     [[1, 2, 3], [4, "hello", 6], [7, 8, 9]], dtype=np.int32
+# )  # z 'hello' nie da się zrobić int32
+# a = np.array([[1, 2, 3], [4, "5", 6], [7, 8, 9]], dtype=np.int32)
+# print(a.dtype)
+# print(a[1][1].dtype)
+# print(a[1, 1])
+
+# d = {"1": "A"}
+# a = np.array([[1, 2, 3], [4, d, 6], [7, 8, "hello"]])
+# print(a.dtype)
+# print(type(a[1][1]))
+
+# a = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]], dtype="<U7")
+# print(a.dtype)
+# print(type(a[1][0]))
